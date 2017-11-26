7e745bcdab831df8e853e6b9d36eca2d014bdcdf
tomonobu <bokotomo@me.com>
2017-11-26 21:22:46 +0900

    リファクタリング

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SwiftTableViewInCollectionVIew
+xibの構成。
+StoryboadにtableViewCellをつけて、その中にcontainerViewつけると落ちるので、xibの構成になっている。
 自分用  
 
 <img src="https://github.com/bokotomo/SwiftTableViewInCollectionVIew/blob/master/show.png" width="100%">  
--- testInstaLikeApp/HomeViewController.swift
+++ testInstaLikeApp/HomeViewController.swift
@@ -1,11 +1,14 @@
+import UIKit
 
 extension HomeViewController:  UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         
         guard let cell = cell as? ShopTableViewCell else { return }
-        
-        //ShopTableViewCell.swiftで設定したメソッドを呼び出す
+        // 毎回デリゲートを更新する
         cell.setCollectionViewDataSourceDelegate(dataSourceDelegate: self, forRow: indexPath.row)
     }
     
@@ -13,19 +16,33 @@ extension HomeViewController:  UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShopTableViewCell", for: indexPath) as! ShopTableViewCell
         
         return cell
-    }
-    
+    }    
 }
 
-
 extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShopCollectionViewCell", for: indexPath) as! ShopCollectionViewCell
+        cell.title.text = "せる \(indexPath.row)"
         
-        cell.shopName.text = homeData.topPickUpData[indexPath.row].shopName
-        cell.shopThumb.image = UIImage.init(named: "shop.png")
-        
+        return cell
+    }
+}
+
+class HomeViewController: UIViewController{
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "ShopTableViewCell", bundle: nil)
+        homeTableView.register(nib, forCellReuseIdentifier: "ShopTableViewCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
